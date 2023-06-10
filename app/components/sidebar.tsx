bb685623a83f6d5d707028a0605b83bbcd5d243a
qiuchao <qc_hao@163.com>
2023-06-10 13:11:24 +0800

    no message

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>My ChatGPT</div>
+        <div className={styles["sidebar-title"]}>51chat.pro</div>
         <div className={styles["sidebar-sub-title"]}>你的个人 AI 助手</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
